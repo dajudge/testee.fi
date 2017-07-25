acfbc16f7ec71b8c821ced0fa04802e0ce413a04
Alex Stockinger <alex@it-stockinger.de>
2017-07-25 16:29:22 +0200

    Publish to https://oss.sonatype.org/content/repositories/snapshots

--- usageTests/maven/build.sh
+++ usageTests/maven/build.sh
@@ -13,4 +13,4 @@ rm -f /tmp/apache-maven.tar.gz
 MAVEN_HOME=$BASE_DIR
 MAVEN_CONFIG="$USER_HOME_DIR/.m2"
 
-$BASE_DIR/bin/mvn clean verify -gs settings.xml -Dnexus.username=$MAVEN_USER -Dnexus.password=$MAVEN_PASSWORD clean install
+$BASE_DIR/bin/mvn clean install
